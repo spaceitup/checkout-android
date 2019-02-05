66bd1d1b104f9cfe3dac61afce46c7fde80bd2a5
sbrouwer <siebe.brouwer@optile.net>
2019-02-05 12:34:43 +0100

    Create ImageHelperTest.java
    Add the ImageHelperTest class for testing the utility class ImageHelper.

---
+++ payment/src/test/java/net/optile/payment/util/ImageHelperTest.java
@@ -0,0 +1,28 @@
+
+/**
+ * Copyright(c) 2012-2019 optile GmbH. All Rights Reserved.
+ * https://www.optile.net
+ *
+ * This software is the property of optile GmbH. Distribution  of  this
+ * software without agreement in writing is strictly prohibited.
+ *
+ * This software may not be copied, used or distributed unless agreement
+ * has been received in full.
+ */
+
+package net.optile.payment.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Class for testing the ImageHelper util class
+ */
+public class ImageHelperTest {
+
+    @Test
+    public void getInstance() {
+        assertNotNull(ImageHelper.getInstance());
+    }
+}
