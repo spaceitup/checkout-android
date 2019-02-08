c5e88e098d2091f8607c09d075a8fa2725856c1b
sbrouwer <siebe.brouwer@optile.net>
2019-02-08 16:00:58 +0100

    PCS-174 javadocs update

--- payment/src/main/java/net/optile/payment/ui/widget/FormWidget.java
+++ payment/src/main/java/net/optile/payment/ui/widget/FormWidget.java
@@ -93,7 +93,7 @@ public abstract class FormWidget {
     }
 
     /**
-     * Set the validation state given the current input value.
+     * Set the validation in this widget given the current input value.
      */
     public void setValidation() {
     }
