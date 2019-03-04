c811898965e81c491037201a4e23d6e29122f633
Pritam Patil <pritam.patil@optile.net>
2019-03-04 10:58:20 +0100

    Added IO Exceptions

--- example/src/androidTest/java/net/optile/payment/network/ListUrlGen.java
+++ example/src/androidTest/java/net/optile/payment/network/ListUrlGen.java
@@ -5,22 +5,23 @@ import android.util.Log;
 import net.optile.payment.core.PaymentException;
 import net.optile.payment.model.ListResult;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Map;
 
 public class ListUrlGen {
 
-    public String getListUrl(String url, String authorization, String listData) throws Exception {
+    public String getListUrl(String url, String authorization, String listData) throws IOException {
         ListConnection conn = new ListConnection();
         try {
             ListResult result = conn.createPaymentSession(url, authorization, listData);
             URL selfUrl = getSelfUrl(result.getLinks());
             if (selfUrl == null) {
-                throw new Exception("Error creating payment session, missing self url");
+                throw new IOException("Error creating payment session, missing self url");
             }
             return selfUrl.toString();
         } catch (PaymentException e) {
-            throw new Exception("Error creating payment session", e);
+            throw new IOException("Error creating payment session", e);
         }
     }
 
--- example/src/androidTest/java/net/optile/payment/tests/PaymentPageTests.java
+++ example/src/androidTest/java/net/optile/payment/tests/PaymentPageTests.java
@@ -2,13 +2,16 @@ package net.optile.payment.tests;
 //package net.optile.payment.network;
 
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
 import net.optile.example.checkout.CheckoutActivity;
+import net.optile.example.checkout.test.R;
 import net.optile.payment.network.ListUrlGen;
+import net.optile.payment.util.PaymentUtils;
 
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -37,29 +40,18 @@ public class PaymentPageTests {
             CheckoutActivity.class);
 
     @Test
-    public void checkPaymentPage() throws Exception {
+    public void checkPaymentPage() throws IOException {
         String jsonBody = loadJsonBody();
         Log.i("unittest", "jsonBody: " + jsonBody);
         ListUrlGen list = new ListUrlGen();
-        //String listUrl = list.getListUrl(uri,auth,jsonBody);
+        String listUrl = list.getListUrl(uri,auth,jsonBody);
     }
 
-    private String loadJsonBody() {
+    private String loadJsonBody() throws IOException {
+        String json = PaymentUtils.readRawResource(InstrumentationRegistry.getContext().getResources(), R.raw.preset);
+        Log.i("pay", "in: " + json);
 
-        String filename = "preset.json";
 
-        JSONParser parser = new JSONParser();
-        Object obj;
-
-        try {
-            obj = parser.parse(new FileReader(filename));
-            return "foo";
-        } catch (IOException e) {
-            Log.w("unittest", e);
-        } catch (ParseException e) {
-            Log.w("unittest", e);
-        }
-        return null;
     }
 
 }
