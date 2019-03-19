963f2715c48e48b50ec86e716be14f7f46778229
Pritam Patil <pritam.patil@optile.net>
2019-03-19 15:00:21 +0100

    copyright text added

--- example/src/androidTest/java/net/optile/payment/tests/CheckoutPageTests.java
+++ example/src/androidTest/java/net/optile/payment/tests/CheckoutPageTests.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2019 optile GmbH
+ * https://www.optile.net
+ *
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more information.
+ */
+
 package net.optile.payment.tests;
 
 
