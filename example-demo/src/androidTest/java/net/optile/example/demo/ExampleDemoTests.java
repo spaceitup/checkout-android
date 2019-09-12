58f626da74c8eb35de88e9d5b0a76a138fcd5af3
Pritam Patil <pritam.patil@optile.net>
2019-09-12 15:06:44 +0200

    PresetFirst:True successful charge test added

--- example-demo/src/androidTest/java/net/optile/example/demo/ExampleDemoTests.java
+++ example-demo/src/androidTest/java/net/optile/example/demo/ExampleDemoTests.java
@@ -40,6 +40,7 @@ import android.view.View;
 import net.optile.example.demo.checkout.CheckoutActivity;
 import net.optile.example.demo.confirm.ConfirmActivity;
 import net.optile.example.demo.settings.SettingsActivity;
+import net.optile.example.demo.summary.SummaryActivity;
 import net.optile.payment.test.service.ListConfig;
 import net.optile.payment.test.service.ListService;
 import net.optile.payment.test.view.ActivityHelper;
@@ -55,7 +56,7 @@ public class ExampleDemoTests {
     public ActivityTestRule<SettingsActivity> settingsActivityRule = new ActivityTestRule<>(SettingsActivity.class);
 
     @Test
-    public void successfulDirectChargeTest() throws JSONException, IOException {
+    public void successfulNoPresetChargeTest() throws JSONException, IOException {
         Intents.init();
 
         ListService service = getListService();
@@ -114,10 +115,80 @@ public class ExampleDemoTests {
         intended(hasComponent(ConfirmActivity.class.getName()));
         onView(withId(R.id.layout_confirm)).check(matches(isDisplayed()));
 
-        IdlingRegistry.getInstance().unregister(chargeIdlingResource);
-        IdlingRegistry.getInstance().unregister(loadIdlingResource);
-        IdlingRegistry.getInstance().unregister(dialogIdlingResource);
+        IdlingRegistry.getInstance().unregister(chargeIdlingResource, loadIdlingResource, dialogIdlingResource);
 
+        Intents.release();
+    }
+
+    @Test
+    public void successfulPresetChargeTest() throws IOException, JSONException {
+        Intents.init();
+
+        ListService service = getListService();
+        ListConfig config = service.createListConfig(net.optile.example.demo.test.R.raw.listtemplate);
+        config.setPresetFirst(true);
+        String listUrl = service.createListUrl(config);
+
+        // enter the listUrl in the settings screen and click the button
+        onView(withId(R.id.layout_settings)).check(matches(isDisplayed()));
+        onView(withId(R.id.input_listurl)).perform(typeText(listUrl));
+        onView(withId(R.id.button_settings)).perform(click());
+
+        // Wait for CheckoutActivity to be visible and click the pay button
+        intended(hasComponent(CheckoutActivity.class.getName()));
+
+        onView(withId(R.id.button_checkout)).perform(PaymentActions.scrollToView(), click());
+
+        // Obtain the PaymentListActivity
+        intended(hasComponent(PaymentListActivity.class.getName()));
+        PaymentListActivity listActivity = (PaymentListActivity) ActivityHelper.getCurrentActivity();
+        IdlingResource dialogIdlingResource = listActivity.getDialogIdlingResource();
+        IdlingResource loadIdlingResource = listActivity.getLoadIdlingResource();
+
+        // Wait for the SimpleIdlingResource until the PaymentList is loaded
+        IdlingRegistry.getInstance().register(loadIdlingResource);
+        Matcher<View> list = withId(R.id.recyclerview_paymentlist);
+        onView(list).check(matches(isDisplayed()));
+
+
+        // Check and fill the payment card
+        onView(list).check(matches(isCardWithTestId(0, "label_header")));
+        onView(list).check(matches(isCardWithTestId(1, "card_group")));
+        onView(list).perform(actionOnItemAtPosition(1, click()));
+        onView(list).perform(actionOnViewInWidget(1, typeText("4111111111111111"), "number", R.id.textinputedittext));
+        onView(list).perform(actionOnViewInWidget(1, typeText("John Doe"), "holderName", R.id.textinputedittext));
+        onView(list).perform(actionOnViewInWidget(1, click(), "expiryDate", R.id.textinputedittext));
+
+        // wait for the DialogIdlingResource until the DateDialog is visible
+        IdlingRegistry.getInstance().register(dialogIdlingResource);
+        onView(withId(R.id.text_button_neutral)).check(matches(isDisplayed()));
+        onView(withId(R.id.numberpicker_year)).perform(setValueInNumberPicker(4));
+        onView(withId(R.id.text_button_neutral)).perform(click());
+
+        // Click on the widget button
+        onView(list).perform(actionOnViewInWidget(1, typeText("123"), "verificationCode", R.id.textinputedittext));
+
+
+        IdlingResource closeIdlingResource = listActivity.getCloseIdlingResource();
+        onView(list).perform(actionOnViewInWidget(1, click(), "buttonWidget", R.id.button));
+        IdlingRegistry.getInstance().register(closeIdlingResource);
+
+        //Wait for the summary activity to load and click on pay button
+        intended(hasComponent(SummaryActivity.class.getName()));
+        onView(withId(R.id.button_pay)).perform(PaymentActions.scrollToView(), click());
+
+        // Check that the ChargePaymentActivity is launched and wait for it to close
+        intended(hasComponent(ChargePaymentActivity.class.getName()));
+        onView(withId(R.id.layout_chargepayment)).check(matches(isDisplayed()));
+        ChargePaymentActivity chargeActivity = (ChargePaymentActivity) ActivityHelper.getCurrentActivity();
+        IdlingResource chargeIdlingResource = chargeActivity.getChargeIdlingResource();
+        IdlingRegistry.getInstance().register(chargeIdlingResource);
+
+        // Check that the confirm activity is shown and that it is displayed
+        intended(hasComponent(ConfirmActivity.class.getName()));
+        onView(withId(R.id.layout_confirm)).check(matches(isDisplayed()));
+
+        IdlingRegistry.getInstance().unregister(chargeIdlingResource, loadIdlingResource, dialogIdlingResource, closeIdlingResource);
         Intents.release();
     }
 
