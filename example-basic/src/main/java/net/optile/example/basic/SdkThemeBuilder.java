b9a14ab909c6e328414810639e27d2decb01ee4e
siebeprojects <siebe.brouwer@optile.net>
2019-10-02 15:18:45 +0200

    Update example-basic with AndroidX theming

--- example-basic/src/main/java/net/optile/example/basic/SdkThemeBuilder.java
+++ example-basic/src/main/java/net/optile/example/basic/SdkThemeBuilder.java
@@ -8,12 +8,7 @@
 
 package net.optile.example.basic;
 
-import net.optile.payment.ui.theme.ChargeParameters;
-import net.optile.payment.ui.theme.DialogParameters;
-import net.optile.payment.ui.theme.ListParameters;
 import net.optile.payment.ui.theme.PaymentTheme;
-import net.optile.payment.ui.theme.ProgressParameters;
-import net.optile.payment.ui.theme.WidgetParameters;
 
 /**
  * Class used to create a custom PaymentTheme for theming the Android SDK Payment page
@@ -39,73 +34,20 @@ final class SdkThemeBuilder {
     }
 
     /**
-     * Construct a new PaymentTheme with the custom dark skin for the Android SDK payment page
+     * Construct a new PaymentTheme with the custom orange skin
      *
      * @return PaymentTheme containing the custom skin
      */
     public static PaymentTheme createCustomTheme() {
-        PaymentTheme.Builder builder = PaymentTheme.createBuilder();
-
-        ListParameters listParams = ListParameters.createBuilder().
-            setPageTheme(R.style.CustomThemePaymentList).
-            setEmptyListLabelStyle(R.style.CustomText_Medium_Light).
-            setSectionHeaderLabelStyle(R.style.CustomText_Medium_Bold_Light).
-            setPresetCardTitleStyle(R.style.CustomText_Medium_Bold).
-            setPresetCardSubtitleStyle(R.style.CustomText_Small).
-            setAccountCardTitleStyle(R.style.CustomText_Medium_Bold).
-            setAccountCardSubtitleStyle(R.style.CustomText_Small).
-            setNetworkCardTitleStyle(R.style.CustomText_Medium_Bold).
-            setPaymentLogoBackground(R.drawable.custom_logobackground).
-            build();
-        builder.setListParameters(listParams);
-
-        ChargeParameters processParams = ChargeParameters.createBuilder().
-            setPageTheme(R.style.CustomThemeProcessPayment).
-            build();
-        builder.setChargeParameters(processParams);
-
-        WidgetParameters widgetParams = WidgetParameters.createBuilder().
-            setTextInputTheme(R.style.CustomThemeTextInput).
-            setButtonTheme(R.style.CustomThemeButton).
-            setButtonLabelStyle(R.style.CustomText_Medium_Bold_Light).
-            setButtonBackground(R.drawable.custom_buttonbackground).
-            setCheckBoxTheme(R.style.CustomThemeCheckBox).
-            setCheckBoxLabelCheckedStyle(R.style.CustomText_Medium).
-            setCheckBoxLabelUncheckedStyle(R.style.CustomText_Medium_Gray).
-            setSelectLabelStyle(R.style.CustomText_Tiny).
+        return PaymentTheme.createBuilder().
+            setPaymentListTheme(R.style.CustomPaymentTheme_PaymentList).
+            setChargePaymentTheme(R.style.CustomPaymentTheme_ChargePayment).
+            setDateDialogTheme(R.style.CustomDialogTheme_Date).
+            setMessageDialogTheme(R.style.CustomDialogTheme_Message).
             setValidationColorOk(R.color.custom_validationok).
             setValidationColorUnknown(R.color.custom_validationunknown).
             setValidationColorError(R.color.custom_validationerror).
-            setInfoLabelStyle(R.style.CustomText_Small).
             setDefaultIconMapping().
-            setDefaultHintDrawable().
             build();
-        builder.setWidgetParameters(widgetParams);
-
-        DialogParameters dialogParams = DialogParameters.createBuilder().
-            setDialogTheme(R.style.CustomThemeDialog).
-            setDateTitleStyle(R.style.CustomText_Medium_Bold).
-            setDateSubtitleStyle(R.style.CustomText_Small_Bold).
-            setMessageTitleStyle(R.style.CustomText_Large_Bold).
-            setMessageDetailsStyle(R.style.CustomText_Medium).
-            setMessageDetailsNoTitleStyle(R.style.CustomText_Medium_Bold).
-            setButtonLabelStyle(R.style.CustomText_Small_Bold_Primary).
-            setImageLabelStyle(R.style.CustomText_Tiny).
-            setSnackbarTextStyle(R.style.CustomText_Small_Light).
-            build();
-        builder.setDialogParameters(dialogParams);
-
-        ProgressParameters progressParams = ProgressParameters.createBuilder().
-            setLoadBackground(R.color.customColorPrimary).
-            setLoadProgressBarColor(R.color.customColorAccent).
-            setSendBackground(R.color.custom_cardbackground).
-            setSendProgressBarColorFront(R.color.customColorPrimary).
-            setSendProgressBarColorBack(R.color.custom_validationunknown).
-            setHeaderStyle(R.style.CustomText_XLarge_Bold).
-            setInfoStyle(R.style.CustomText_Medium).
-            build();
-        builder.setProgressParameters(progressParams);
-
-        return builder.build();
     }
 }
