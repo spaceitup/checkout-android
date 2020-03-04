ff8399fa37f3379532684251f851a798af6aafa1
siebeprojects <siebe.brouwer@optile.net>
2020-03-04 14:42:03 +0100

    PCX-428: Update unit tests
    Update the unit tests for validating the new Interaction code and reasons and redirect types.

---
+++ payment/src/test/java/net/optile/payment/model/RedirectTypeTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2019 optile GmbH
+ * https://www.optile.net
+ *
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more information.
+ */
+
+package net.optile.payment.model;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * The RedirectType test
+ */
+public class RedirectTypeTest {
+
+    @Test
+    public void isRedirectType_invalidValue_false() {
+        assertFalse(RedirectType.isValid("foo"));
+    }
+
+    @Test
+    public void isRedirectType_validValue_true() {
+        assertTrue(RedirectType.isValid(RedirectType.SUMMARY));
+        assertTrue(RedirectType.isValid(RedirectType.RETURN));
+        assertTrue(RedirectType.isValid(RedirectType.CANCEL));
+        assertTrue(RedirectType.isValid(RedirectType.PROVIDER));
+        assertTrue(RedirectType.isValid(RedirectType.HANDLER3DS2));
+    }
+}
