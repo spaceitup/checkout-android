d39661debb2c57d00f3060b04e20698a6247ab0d
Siebe Brouwer <siebe@siebeprojects.com>
2020-11-16 13:15:26 +0100

    PCX-1147: license link test

--- payment/src/main/java/net/optile/payment/core/PaymentNetworkCodes.java
+++ payment/src/main/java/net/optile/payment/core/PaymentNetworkCodes.java
@@ -3,7 +3,7 @@
  * https://www.optile.net
  *
  * This file is open source and available under the MIT license.
- * See the LICENSE file for more information.
+ * See the <a href="http://google.com">LICENSE</a> file for more information.
  */
 
 package net.optile.payment.core;
