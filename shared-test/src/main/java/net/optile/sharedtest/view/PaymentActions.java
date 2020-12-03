9e80fe6d188e36606f3d0c8c748b19c172d5653f
Siebe Brouwer <siebe@siebeprojects.com>
2020-12-03 11:02:18 +0100

    PCX-1208: Create shared library for testing
    This shared library replaces the shared test classes previously stored in the payment module.

---
+++ shared-test/src/main/java/net/optile/sharedtest/view/PaymentActions.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2020 optile GmbH
+ * https://www.optile.net
+ *
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more information.
+ */
+
+package net.optile.sharedtest.view;
+
+import static androidx.test.espresso.intent.Checks.checkNotNull;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.anyOf;
+
+import org.hamcrest.Matcher;
+
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.NumberPicker;
+import android.widget.ScrollView;
+import androidx.core.widget.NestedScrollView;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.PerformException;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.ScrollToAction;
+import androidx.test.espresso.matcher.ViewMatchers;
+import net.optile.payment.ui.list.PaymentCardViewHolder;
+import net.optile.payment.ui.widget.FormWidget;
+
+/**
+ * Class providing helper methods for performing actions on the PaymentList.
+ */
+public final class PaymentActions {
+
+    /**
+     * Perform the action on a View inside a card widget at the given position.
+     *
+     * @param position of the card inside the RecyclerView
+     * @param action to be performed on the View
+     * @param widgetName name of the widget inside the card, i.e. holderName or number
+     * @param viewResId resource ID of the View
+     * @return the newly created ViewAction
+     */
+    public static ViewAction actionOnViewInWidget(int position, final ViewAction action, final String widgetName, final int viewResId) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return allOf(isAssignableFrom(RecyclerView.class), isDisplayed());
+            }
+
+            @Override
+            public String getDescription() {
+                return String.format("perform action on view in widget %s at position %d", widgetName, position);
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                RecyclerView.ViewHolder viewHolder = ((RecyclerView) view).findViewHolderForAdapterPosition(position);
+                checkNotNull(viewHolder);
+
+                if (!(viewHolder instanceof PaymentCardViewHolder)) {
+                    throw createPerformException("ViewHolder is not of type PaymentCardViewHolder");
+                }
+                FormWidget widget = ((PaymentCardViewHolder) viewHolder).getFormWidget(widgetName);
+                if (widget == null) {
+                    throw createPerformException(String.format("Widget %s could not be found inside card", widgetName));
+                }
+                View formView = widget.getRootView().findViewById(viewResId);
+                if (formView == null) {
+                    throw createPerformException("Could not find the View inside the Widget: " + widgetName);
+                }
+                action.perform(uiController, formView);
+            }
+        };
+    }
+
+    /**
+     * Scroll to the view action
+     *
+     * @return the newly created ViewAction
+     */
+    public static ViewAction scrollToView() {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return allOf(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE),
+                    isDescendantOfA(anyOf(isAssignableFrom(ScrollView.class),
+                        isAssignableFrom(HorizontalScrollView.class),
+                        isAssignableFrom(NestedScrollView.class))));
+            }
+
+            @Override
+            public String getDescription() {
+                return "scroll to view action";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                new ScrollToAction().perform(uiController, view);
+            }
+        };
+    }
+
+    /**
+     * Scroll to the view action
+     *
+     * @param value
+     * @return the newly created ViewAction
+     */
+    public static ViewAction setValueInNumberPicker(final int value) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return ViewMatchers.isAssignableFrom(NumberPicker.class);
+            }
+
+            @Override
+            public String getDescription() {
+                return String.format("Set the value %d of a NumberPicker", value);
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                ((NumberPicker) view).setValue(value);
+            }
+        };
+    }
+
+    private static PerformException createPerformException(String description) {
+        return new PerformException.Builder()
+            .withActionDescription(description)
+            .build();
+    }
+}
+
---
+++ shared-test/src/main/java/net/optile/sharedtest/view/PaymentMatchers.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2020 optile GmbH
+ * https://www.optile.net
+ *
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more information.
+ */
+
+package net.optile.sharedtest.view;
+
+import static androidx.test.espresso.intent.Checks.checkNotNull;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import android.view.View;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.matcher.BoundedMatcher;
+import net.optile.payment.ui.list.PaymentCardViewHolder;
+import net.optile.payment.ui.widget.FormWidget;
+
+/**
+ * Class holding espresso matchers to test the Android SDK
+ */
+public final class PaymentMatchers {
+
+    /**
+     * Check the card testId at the absolute position in the RecyclerView.
+     *
+     * @param position of the card in the RecyclerView
+     * @param testId compared to the content description of the root view
+     * @return newly created View Matcher
+     */
+    public static Matcher<View> isCardWithTestId(int position, String testId) {
+        checkNotNull(testId);
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText(String.format("match holder testId %s at position %d", testId, position));
+            }
+
+            @Override
+            protected boolean matchesSafely(final RecyclerView recyclerView) {
+                RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForAdapterPosition(position);
+                if (viewHolder == null) {
+                    return false;
+                }
+                return testId.equals(viewHolder.itemView.getContentDescription().toString());
+            }
+        };
+    }
+
+    /**
+     * Check the view inside a card at the absolute position in the RecyclerView.
+     *
+     * @param position of the card in the RecyclerView
+     * @param viewMatcher to match the view against
+     * @param viewResId resource ID of the view
+     * @return newly created View Matcher
+     */
+    public static Matcher<View> isViewInCard(int position, Matcher<View> viewMatcher, int viewResId) {
+        checkNotNull(viewMatcher);
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText(String.format("match view in holder at position %d", position));
+            }
+
+            @Override
+            public boolean matchesSafely(final RecyclerView recyclerView) {
+                RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForAdapterPosition(position);
+                if (viewHolder == null) {
+                    return false;
+                }
+                View view = viewHolder.itemView.findViewById(viewResId);
+                if (view == null) {
+                    return false;
+                }
+                return viewMatcher.matches(view);
+            }
+        };
+    }
+
+    /**
+     * Check the view inside a card widget at the absolute position in the RecyclerView.
+     *
+     * @param position of the card in the RecyclerView
+     * @param viewMatcher to match the view against
+     * @param widgetName the name of the widget, i.e. number or holderName
+     * @param viewResId resource ID of the view
+     * @return newly created View Matcher
+     */
+    public static Matcher<View> isViewInWidget(final int position, final Matcher<View> viewMatcher, final String widgetName,
+        final int viewResId) {
+        checkNotNull(viewMatcher);
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText(String.format("match view in widget %s at position %d", widgetName, position));
+            }
+
+            @Override
+            public boolean matchesSafely(RecyclerView recyclerView) {
+                RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForAdapterPosition(position);
+                if (viewHolder == null) {
+                    return false;
+                }
+                FormWidget widget = ((PaymentCardViewHolder) viewHolder).getFormWidget(widgetName);
+                if (widget == null) {
+                    return false;
+                }
+                View formView = widget.getRootView().findViewById(viewResId);
+                if (formView == null) {
+                    return false;
+                }
+                return viewMatcher.matches(formView);
+            }
+        };
+    }
+
+    public static Matcher<View> hasTextInputLayoutHint(final String expectedHint) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view instanceof TextInputLayout)) {
+                    return false;
+                }
+                CharSequence hintSequence = ((TextInputLayout) view).getHint();
+                String inputHint = "";
+                if (hintSequence != null) {
+                    inputHint = hintSequence.toString();
+                }
+                return expectedHint.equals(inputHint);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+            }
+        };
+    }
+
+    public static Matcher<View> hasTextInputLayoutError(final String expectedError) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view instanceof TextInputLayout)) {
+                    return false;
+                }
+                CharSequence errorSequence = ((TextInputLayout) view).getError();
+                String inputError = "";
+                if (errorSequence != null) {
+                    inputError = errorSequence.toString();
+                }
+                return expectedError.equals(inputError);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+            }
+        };
+    }
+}
