28a7ac6bf9078decfb7abe2833a6cb620e6afc5f
Siebe Brouwer <siebe@siebeprojects.com>
2021-01-22 17:09:16 +0100

    PCX-1277: Update tests

--- payment/src/test/java/net/optile/payment/core/PaymentInputTypeTest.java
+++ payment/src/test/java/net/optile/payment/core/PaymentInputTypeTest.java
@@ -22,15 +22,25 @@ public class PaymentInputTypeTest {
 
     @Test
     public void isPaymentInputType_validValue_true() {
-        assertTrue(PaymentInputType.isValid(PaymentInputType.ACCOUNT_NUMBER));
         assertTrue(PaymentInputType.isValid(PaymentInputType.HOLDER_NAME));
-        assertTrue(PaymentInputType.isValid(PaymentInputType.EXPIRY_DATE));
+        assertTrue(PaymentInputType.isValid(PaymentInputType.ACCOUNT_NUMBER));
+        assertTrue(PaymentInputType.isValid(PaymentInputType.BANK_CODE));
+        assertTrue(PaymentInputType.isValid(PaymentInputType.BANK_NAME));
+        assertTrue(PaymentInputType.isValid(PaymentInputType.BIC));
+        assertTrue(PaymentInputType.isValid(PaymentInputType.BRANCH));
+        assertTrue(PaymentInputType.isValid(PaymentInputType.CITY));
         assertTrue(PaymentInputType.isValid(PaymentInputType.EXPIRY_MONTH));
         assertTrue(PaymentInputType.isValid(PaymentInputType.EXPIRY_YEAR));
-        assertTrue(PaymentInputType.isValid(PaymentInputType.VERIFICATION_CODE));
-        assertTrue(PaymentInputType.isValid(PaymentInputType.BANK_CODE));
+        assertTrue(PaymentInputType.isValid(PaymentInputType.EXPIRY_DATE));
         assertTrue(PaymentInputType.isValid(PaymentInputType.IBAN));
-        assertTrue(PaymentInputType.isValid(PaymentInputType.BIC));
+        assertTrue(PaymentInputType.isValid(PaymentInputType.LOGIN));
+        assertTrue(PaymentInputType.isValid(PaymentInputType.OPTIN));
+        assertTrue(PaymentInputType.isValid(PaymentInputType.PASSWORD));
+        assertTrue(PaymentInputType.isValid(PaymentInputType.VERIFICATION_CODE));
+        assertTrue(PaymentInputType.isValid(PaymentInputType.CUSTOMER_BIRTHDAY));
+        assertTrue(PaymentInputType.isValid(PaymentInputType.CUSTOMER_BIRTHMONTH));
+        assertTrue(PaymentInputType.isValid(PaymentInputType.CUSTOMER_BIRTHYEAR));
+        assertTrue(PaymentInputType.isValid(PaymentInputType.INSTALLMENT_PLANID));
         assertTrue(PaymentInputType.isValid(PaymentInputType.ALLOW_RECURRENCE));
         assertTrue(PaymentInputType.isValid(PaymentInputType.AUTO_REGISTRATION));
     }
--- payment/src/test/java/net/optile/payment/form/OperationTest.java
+++ payment/src/test/java/net/optile/payment/form/OperationTest.java
@@ -43,23 +43,33 @@ public class OperationTest {
     public void putValue_invalidName_exception() throws PaymentException {
         URL url = TestUtils.createTestURL("http://localhost/charge");
         Operation operation = new Operation("VISA", "CREDIT_CARD", "CHARGE", url);
-        operation.putValue(null, "Foo");
+        operation.putStringValue(null, "Foo");
     }
 
     @Test
     public void putValue_success() throws PaymentException, JSONException {
         URL url = TestUtils.createTestURL("http://localhost/charge");
         Operation operation = new Operation("VISA", "CREDIT_CARD", "CHARGE", url);
-        operation.putValue(PaymentInputType.ACCOUNT_NUMBER, "accountnumber123");
-        operation.putValue(PaymentInputType.HOLDER_NAME, "John Doe");
-        operation.putValue(PaymentInputType.EXPIRY_MONTH, 12);
-        operation.putValue(PaymentInputType.EXPIRY_YEAR, 2019);
-        operation.putValue(PaymentInputType.VERIFICATION_CODE, "123");
-        operation.putValue(PaymentInputType.BANK_CODE, "bankcode123");
-        operation.putValue(PaymentInputType.IBAN, "iban123");
-        operation.putValue(PaymentInputType.BIC, "bic123");
-        operation.putValue(PaymentInputType.ALLOW_RECURRENCE, "true");
-        operation.putValue(PaymentInputType.AUTO_REGISTRATION, "true");
+        operation.putStringValue(PaymentInputType.HOLDER_NAME, "John Doe");
+        operation.putStringValue(PaymentInputType.ACCOUNT_NUMBER, "accountnumber123");
+        operation.putStringValue(PaymentInputType.BANK_CODE, "bankcode123");
+        operation.putStringValue(PaymentInputType.BANK_NAME, "bankname123");
+        operation.putStringValue(PaymentInputType.BIC, "bic123");
+        operation.putStringValue(PaymentInputType.BRANCH, "branch123");
+        operation.putStringValue(PaymentInputType.CITY, "city123");
+        operation.putStringValue(PaymentInputType.EXPIRY_MONTH, "12");
+        operation.putStringValue(PaymentInputType.EXPIRY_YEAR, "2019");
+        operation.putStringValue(PaymentInputType.IBAN, "iban123");
+        operation.putStringValue(PaymentInputType.LOGIN, "login123");
+        operation.putBooleanValue(PaymentInputType.OPTIN, true);
+        operation.putStringValue(PaymentInputType.PASSWORD, "password123");
+        operation.putStringValue(PaymentInputType.VERIFICATION_CODE, "123");
+        operation.putStringValue(PaymentInputType.CUSTOMER_BIRTHDAY, "3");
+        operation.putStringValue(PaymentInputType.CUSTOMER_BIRTHMONTH, "12");
+        operation.putStringValue(PaymentInputType.CUSTOMER_BIRTHYEAR, "72");
+        operation.putStringValue(PaymentInputType.INSTALLMENT_PLANID, "72");
+        operation.putBooleanValue(PaymentInputType.ALLOW_RECURRENCE, true);
+        operation.putBooleanValue(PaymentInputType.AUTO_REGISTRATION, true);
 
         BrowserData browserData = new BrowserData();
         browserData.setJavaEnabled(true);
