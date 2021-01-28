c7cbbbc514a5b72746a9513288e575fc44bf6e91
Siebe Brouwer <siebe@siebeprojects.com>
2021-01-28 16:24:53 +0100

    PCX-1271: Update copyright status source files

--- payment/src/main/java/com/payoneer/mrs/payment/form/BrowserDataBuilder.java
+++ payment/src/main/java/com/payoneer/mrs/payment/form/BrowserDataBuilder.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2021 Payoneer Germany GmbH
- * https://payoneer.com
+ * Copyright (c) 2020 Payoneer Germany GmbH
+ * https://www.payoneer.com
  *
  * This file is open source and available under the MIT license.
  * See the LICENSE file for more information.
--- payment/src/main/java/com/payoneer/mrs/payment/model/FormData.java
+++ payment/src/main/java/com/payoneer/mrs/payment/model/FormData.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2020 optile GmbH
- * https://www.optile.net
+ * Copyright (c) 2020 Payoneer Germany GmbH
+ * https://www.payoneer.com
  *
  * This file is open source and available under the MIT license.
  * See the LICENSE file for more information.
